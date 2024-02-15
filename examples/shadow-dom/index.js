062f305297aa95a7bc8a179e254bf069e1efd301
jwilliams720 <jwilliams720@bloomberg.net>
2024-02-15 13:56:58 +0000

    shadow DOM and index for root

---
+++ examples/shadow-dom/index.js
@@ -0,0 +1,16 @@
+const observer = new PerformanceObserver((list) => {
+  list.getEntries().forEach((entry) => {
+    console.log(entry);
+  });
+});
+
+observer.observe({ type: "element", buffered: true });
+
+window.setTimeout(() => {
+  const host = document.querySelector("#host");
+  const shadow = host.shadowRoot;
+  const paragraph = document.createElement("p");
+  paragraph.textContent =
+    '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."';
+  shadow.appendChild(paragraph);
+}, 5000);
